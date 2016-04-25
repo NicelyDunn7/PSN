8393ce51a49fd8079bb738024f029ad464510d3a
dpdunn10 <dpdunn10@gmail.com>
2016-04-24 23:21:15 -0500

    Create dbcreds.php

---
+++ controllers/dbcreds.php
@@ -0,0 +1,27 @@
+<?php
+
+        if (!isset($_SERVER['HTTPS']) || !$_SERVER['HTTPS']) { // if request is not secure, redirect to secure url
+           $url = 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+           header('Location: ' . $url);
+        }
+
+        session_start();
+
+        if(strcmp($_SESSION['type'],'user') === 0){
+
+        } else if(strcmp($_SESSION['type'], 'business') === 0){
+
+        }else{
+          header('Location: https://mizseng.centralus.cloudapp.azure.com/index.php');
+        }
+
+        //Connect to the MySQL Account on Azure Server
+        $hostname = "";
+        $username = "";
+        $password = "";
+        $dbname = "";
+        $link = new mysqli($hostname, $username, $password, $dbname);
+        if ($link->connect_error) {
+                die("Connection failed: " . $link->connect_error);
+        }
+?>
